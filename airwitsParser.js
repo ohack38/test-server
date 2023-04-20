e9d95460d18c1aab710b8640bc9452e97a5d227c
ohack38 <oscarhackman@Oscar-MacBook-Air.local>
2023-04-20 13:55:09 +0300

    airwits complete

--- airwitsParser.js
+++ airwitsParser.js
@@ -1,12 +1,67 @@
-const byteSize = str => new Blob([str]).size;
+const { readUInt4 } = require('uint4');
+const { Buffer } = require('node:buffer')
 
-const parser = (data) => {
+const parser = (body) => {
+    try {
+        const id = body.id
+        const time = body.timestamp
+        const type = body.type
 
-    const bytes = byteSize(data)
+        let buffer = Buffer.from(body.data, 'hex');
 
-    console.log('Got hex', data, 'bytes: ', bytes)
-    //https://help.tago.io/portal/en/community/topic/how-to-build-a-lorawan-sigfox-payload-parser
+        const byteSize = Buffer.byteLength(buffer)
 
+        if (byteSize == 10) {
+            // MEASUREMENT
+            const parsed = {
+                'id': id,
+                'mac': id,
+                'firmware_version': readUInt4(buffer,0),
+                'temperature': buffer.readInt16BE(1) / 10,
+                'humidity': buffer.readUInt8(3),
+                'pm1': buffer.readUInt16BE(4) / 10,
+                'pm25': buffer.readUInt16BE(6) / 10,
+                'pm10': buffer.readUInt16BE(8) / 10,
+                'time': time,
+                'type': type,
+                'msg_type': 'measurement'
+            }
+            return parsed
 
+        } else if(byteSize == 11) {
+           // SYSTEM FUNCTION 1
+            const parsed = {
+                'id': id,
+                'mac': id,
+                'firmware_version': readUInt4(buffer,0),
+                'msg1000': buffer.readUint8(1),
+                'bat_voltage': buffer.readUInt8(2) / 50,
+                'config': buffer.readBigUInt64BE(3),
+                'time': time,
+                'type': type,
+                'msg_type': 'info'
+            }
+            return parsed
+
+        } else if(byteSize == 5) {
+           // SYSTEM FUNCTION 0
+           const parsed = {
+                'id': id,
+                'mac': id,
+                'firmware_version': readUInt4(buffer,0),
+                'radio_zone': readUInt4(buffer,1),
+                'hw_release': buffer.readUInt8(2),
+                'device_type': buffer.readUInt8(3),
+                'build': buffer.readUInt32BE(4),
+                'time': time,
+                'type': type,
+                'msg_type': 'start'
+            }
+            return parsed 
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 exports.parser = parser
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 
-const parser = require('./parser')
+//const parser = require('./parser')
 const airwitsParser = require('./airwitsParser')
 const app = express();
 
@@ -9,17 +9,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // MILESIGHT IO SENSOR
-app.post('/lora', (req, res) => {
-    //console.log('Got body:', req.body);
-    res.sendStatus(200);
-    const uplink = req.body.DevEUI_uplink
-    if(uplink){
-        console.log('data:', uplink.payload_hex, uplink.FPort, uplink.Time)
-        const parsed = parser.Decoder(uplink.payload_hex, uplink.FPort)
-        console.log('parsed: ', parsed)
-    }
+// app.post('/lora', (req, res) => {
+//     //console.log('Got body:', req.body);
+//     res.sendStatus(200);
+//     const uplink = req.body.DevEUI_uplink
+//     if(uplink){
+//         console.log('data:', uplink.payload_hex, uplink.FPort, uplink.Time)
+//         const parsed = parser.Decoder(uplink.payload_hex, uplink.FPort)
+//         console.log('parsed: ', parsed)
+//     }
     
-});
+// });
 
 // ERS-sound
 app.post('/ers-sound', (req, res) => {
@@ -38,8 +38,9 @@ app.post('/ers-eye', (req, res) => {
 // airwits 
 app.post('/airwits', (req, res) => {
     //console.log('Got body:', req.body);
-    let data = req.body.data
-    airwitsParser.parser(data)
+    let body = req.body
+    let parsed = airwitsParser.parser(body)
+    console.log('parsed',parsed)
     res.sendStatus(200);
     
 });
@@ -49,8 +50,6 @@ app.post('/', (req, res) => {
     res.sendStatus(200);
     
 });
-
-app.get("/", (req, res) => res.send("Hello World!"));
 
 
 app.listen(80, () => console.log("Server listening on port 80!"));
